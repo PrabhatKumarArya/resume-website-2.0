9c8192e5f2787589637a421244a5b1afc3c79f72
Prabhat Kumar Arya <prabhatkumararya9@gmail.com>
2026-07-19 16:45:34 +0530

    Improved Configurations FOr Auto Mail Feature

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
+import dns from "dns";
 
 dotenv.config();
 
@@ -10,6 +11,10 @@ const getTransporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  family: 4, // Force IPv4
+  tls: {
+    rejectUnauthorized: false,
   },
   connectionTimeout: 30000,
   greetingTimeout: 30000,
