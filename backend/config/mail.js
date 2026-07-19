38e6196b8df14b12e2d537f68927f7d150e5dc9f
Prabhat Kumar Arya <prabhatkumararya9@gmail.com>
2026-07-19 17:32:47 +0530

    Updated Configurations For Auto Reply Feature

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,29 +1,23 @@
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
-import dns from "dns";
 
 dotenv.config();
 
-dns.setDefaultResultOrder("ipv4first");
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
 transporter.verify((err) => {
   if (err) {
-    console.error(err);
+    console.error("SMTP Error:", err);
   } else {
-    console.log("Mail server ready");
+    console.log("✅ Brevo SMTP Connected");
   }
 });
 
